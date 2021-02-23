7a4517b9733cdfa6eafa76b5f430ebe5a36cb6e3
Nathan Bean <nhbean@ksu.edu>
2021-02-23 11:51:51 -0600

    Initial commit

---
+++ PhysicsExampleDGame/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsExampleDGame
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// The width of the game world
+        /// </summary>
+        public static int GAME_WIDTH = 750;
+
+        /// <summary>
+        /// The height of hte game world
+        /// </summary>
+        public static int GAME_HEIGHT = 360;
+    }
+}
